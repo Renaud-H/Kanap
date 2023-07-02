0bd2bbcba2fd97c0e4227f7bd3a94dca35e22aaf
Renaud-H <theqof@hotmail.fr>
2023-07-02 07:46:15 +0200

    commit

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,16 @@
 console.log("Index");
 // Récupération depuis l'api
-fetch("http://localhost:3000/api/products/");
+fetch("http://localhost:3000/api/products/")
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      console.log("Erreur");
+    }
+  })
+  .then(function (itemsList) {
+    console.log(itemsList);
+  });
+
+let section = document.getElementById("items");
+console.log(section);
