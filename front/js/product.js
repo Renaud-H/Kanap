cc916bbcaa0685188e7678e8c45428903c5b97e5
Renaud-H <theqof@hotmail.fr>
2023-07-06 02:25:48 +0200

    product récupération id dans l'adresse html

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,9 @@
-bla
+console.log("Product");
+console.log(window.location.href);
+
+var str = window.location.href;
+var url = new URL(str);
+var id = url.searchParams.get("id");
+console.log(id);
+
+fetch("http://localhost:3000/api/products/");
