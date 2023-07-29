024b711a1e3664a2a9b9e66a40e15880c786e7a9
Renaud-H <theqof@hotmail.fr>
2023-07-29 18:11:03 +0200

    Btn AddtoCart

--- front/js/product.js
+++ front/js/product.js
@@ -4,14 +4,22 @@ console.log(window.location.href);
 var str = window.location.href;
 var url = new URL(str);
 var id = url.searchParams.get("id");
+
+// Vérification de l'ID
+id = parseInt(id);
+if (Number.isNaN(id)) {
+  alert("Erreur, pas d'article sélectionné");
+}
+
 console.log(id);
 
+// Récupération depuis l'api
 fetch("http://localhost:3000/api/products/")
   .then(function (apiPromise) {
     if (apiPromise.ok) {
       return apiPromise.json();
     } else {
-      console.log("Erreur");
+      console.log("Erreur sur la récupération de l'API");
     }
   })
   .then((itemsList) => {
@@ -36,12 +44,7 @@ function showItem(itemsList) {
   price.innerText = `${itemsList[id].price}`;
   description.innerText = `${itemsList[id].description}`;
 
-  console.log("Ciblage du DOM");
-  console.log(color_choice);
-  console.log("Liste des Colors");
-  console.log(itemsList[id].colors);
   // Boucle for pour choisir les couleurs
-
   for (let color in itemsList[id].colors) {
     color_choice.innerHTML += `<option value="${color}">${itemsList[id].colors[color]}</option>`;
     // Option value = (mettre valeur numérique ou chaîne de caractère)
@@ -53,16 +56,52 @@ function GetQuantity() {
   return document.getElementById("quantity").value;
 }
 function GetColor() {
-  return document.getElementById("colors").value ;
+  return document.getElementById("colors").value;
 }
 
+let boutonPanier = document.getElementById("addToCart");
+boutonPanier.addEventListener("click", AjouterPanier);
 
-
-function AjouterPanier(){
-  // id  
+// Enregistrement de l'article en cours dans le panier
+function AjouterPanier() {
+  // id
   let Q = GetQuantity();
   let C = GetColor();
+  if (controlerDonnees(id, Q, C)) enregistrerDonnees();
 }
 
-let BoutonPanier = document.getElementById("addToCart");
-BoutonPanier.addEventListener("click", AjouterPanier());
+//
+function controlerDonnees(id, quantity, color) {
+  // Contrôle de l'id
+  if (id >= 0) {
+    console.log("Contrôle de l'ID ok");
+  } else {
+    console.log("Le contrôle de l'ID n'est pas bon");
+    return false;
+  }
+  // Contrôle de la quantité
+  if (quantity > 0 && quantity < 101) {
+    console.log("Contrôle de la quantité ok");
+  } else {
+    console.log("Le contrôle de la quantité n'est pas bon");
+    return false;
+  }
+  // Contrôle de la couleur
+  if (color >= 0) {
+    console.log("Contrôle de la couleur ok");
+  } else {
+    console.log("Le contrôle de la couleur n'est pas bon");
+    return false;
+  }
+  //si q <0 alors erreur
+}
+function enregistrerDonnees() {}
+
+/*
+localStorage
+get
+
+Q C
+
+créer un array
+*/
