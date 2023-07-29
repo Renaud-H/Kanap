f5b6a52ed50d6550ddb58886f3978e9dbfc27db4
Renaud-H <theqof@hotmail.fr>
2023-07-29 19:31:15 +0200

    Travail sur panier & localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -61,16 +61,17 @@ function GetColor() {
 
 let boutonPanier = document.getElementById("addToCart");
 boutonPanier.addEventListener("click", AjouterPanier);
+// AjouterPanier n'a pas de parenthèses car c'est le nom de la fonction que l'on passe en argument.
 
 // Enregistrement de l'article en cours dans le panier
 function AjouterPanier() {
   // id
   let Q = GetQuantity();
   let C = GetColor();
-  if (controlerDonnees(id, Q, C)) enregistrerDonnees();
+  if (controlerDonnees(id, Q, C)) enregistrerDonnees(id, Q, C);
 }
 
-//
+// Contrôle des données
 function controlerDonnees(id, quantity, color) {
   // Contrôle de l'id
   if (id >= 0) {
@@ -80,28 +81,55 @@ function controlerDonnees(id, quantity, color) {
     return false;
   }
   // Contrôle de la quantité
+  if (Number.isNaN(quantity)) return false;
   if (quantity > 0 && quantity < 101) {
     console.log("Contrôle de la quantité ok");
   } else {
-    console.log("Le contrôle de la quantité n'est pas bon");
+    alert("Veuillez saisir une quantité");
     return false;
   }
   // Contrôle de la couleur
+  if (color == "") {
+    alert("Veuillez saisir une couleur");
+    return false;
+  }
   if (color >= 0) {
     console.log("Contrôle de la couleur ok");
   } else {
     console.log("Le contrôle de la couleur n'est pas bon");
     return false;
   }
-  //si q <0 alors erreur
+  return true;
 }
-function enregistrerDonnees() {}
+// Enregistrement des données
+function enregistrerDonnees(id, quantity, color) {
+  // Lire LocalStorage
+  let panierLocal = localStorage.getItem("Panier");
+  variablePanier = JSON.parse(panierLocal);
+  console.log("Lecture du Panier", variablePanier);
 
-/*
-localStorage
-get
+  if (variablePanier == null) {
+    variablePanier = [];
+  }
 
-Q C
+  console.log(variablePanier);
 
-créer un array
-*/
+  let ligne = dejaPresent(id, color);
+  if (ligne >= 0) {
+    variablePanier[ligne][1] = variablePanier[ligne][1] + quantity;
+  } else variablePanier.push([id, quantity, color]);
+
+  // Additionner les données
+
+  // Enregistrer LocalStorage
+  localStorage.setItem("Panier", JSON.stringify(variablePanier));
+
+  function dejaPresent(id, color) {
+    let compteur = 0;
+    for (let line in variablePanier) {
+      if (line[0] == id && line[2] == color) return compteur;
+      compteur++;
+    }
+    return -1;
+  }
+}
