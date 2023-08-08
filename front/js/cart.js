fb5b7b225d88e7556111f8b4486df68ca2d3f492
Renaud-H <theqof@hotmail.fr>
2023-08-08 14:38:00 +0200

    page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,7 +2,7 @@ console.log("Cart");
 
 showCart(); //affichage du panier
 
-//récupération du panier depuis le localStorage
+// récupération du panier depuis le localStorage
 function getCart() {
     let cart = JSON.parse(localStorage.getItem("cart"));
     //si absence de panier, création d'un tableau vide
@@ -11,4 +11,21 @@ function getCart() {
     }
     console.log(cart);
     return cart;
+}
+// Enregistrement des données
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+// Affichage du panier
+function showCart() {
+    let cart = getCart(); // Récupération du panier
+    // Triage par id
+    cart.sort((a, b) => {
+        if (a.id < b.id)
+            return -1;
+        if (a.id > b.id)
+            return 1;
+        return 0;
+    })
+    console.log(cart);
 }
